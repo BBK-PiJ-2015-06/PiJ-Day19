51ebd915f7912004267af32cd65bc7daefd1ece0
unknown <gshian01@CSPC023.dcs.bbk.ac.uk>
2015-12-03 12:00:18 +0000

    Solutions for exercise 1 completed.

--- Exercise 1/Exercise1.java
+++ Exercise 1/Exercise1.java
@@ -11,5 +11,13 @@ public class Exercise1 {
 		//array sorted by length(i.e. longest to shortest)
 		Arrays.sort(stringArray, (s1, s2) -> s2.length() - s1.length());
 		System.out.println(Arrays.asList(stringArray));
+		
+		//array sorted by first character
+		Arrays.sort(stringArray, (s1, s2) -> s1.charAt(0) - s2.charAt(0));
+		System.out.println(Arrays.asList(stringArray));
+		
+		//array sorted by strings containing "e"
+		Arrays.sort(stringArray, (s1, s2) -> s2.indexOf('e') - s1.indexOf('e')); //indexOf(char) returns -1 if character is not present in string
+		System.out.println(Arrays.asList(stringArray));
 	}
 }
